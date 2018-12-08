c71409c9397f7a95bc7ad6f267aec63a3113f5a8
Misha Marinenko <a0x77@icloud.com>
2018-12-08 12:04:34 +0200

    travis edit

--- installXenial.sh
+++ installXenial.sh
@@ -2,7 +2,7 @@
 git submodule update --init --recursive
 sudo apt-get install make unrar-free autoconf automake libtool gcc g++ gperf \
     flex bison texinfo gawk ncurses-dev libexpat-dev python-dev python python-serial \
-    sed git unzip bash help2man wget bzip2 esptool libtool-bin -y
+    sed git unzip bash help2man wget bzip2 esptool libtool-bin gperf bison flex texinfo -y
 git clone --recursive https://github.com/pfalcon/esp-open-sdk.git
 cd esp-open-sdk
 make toolchain esptool libhal STANDALONE=n
