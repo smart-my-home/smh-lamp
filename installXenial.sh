b8f82034c7afa86993ba2b2ad3f28cb05849d6a2
Misha Marinenko <a0x77@icloud.com>
2018-12-08 12:22:05 +0200

    instalXenial.sh

--- installXenial.sh
+++ installXenial.sh
@@ -1,4 +1,7 @@
 #/bin/sh
+apt update
+apt install sudo -y
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu/ trusty multiverse" && sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu/ trusty-updates multiverse"
 git submodule update --init --recursive
 sudo apt-get install make unrar-free autoconf automake libtool gcc g++ gperf \
     flex bison texinfo gawk ncurses-dev libexpat-dev python-dev python python-serial \
